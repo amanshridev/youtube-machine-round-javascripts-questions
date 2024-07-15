244558829651ec8cac171f8f347dfa3967d2758e
amanshridev <mailtoamanshri@proton.me>
2024-07-15 22:46:29 +0530

    added tsk 4

---
+++ task_04/index.js
@@ -0,0 +1,4 @@
+//Question 1: Refrance Type in Js
+//Question 2: What is the difference between a primitive and a reference type?
+//Question 3: What is the difference between a let and var?
+//Question 4: What is the difference quesrySelectorAll and getElementByClass?
